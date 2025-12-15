3df3b66e8f7bfff010792dd58bfd5a0f22d54900
徐辰 <windoze@0d0a.com>
2025-12-15 15:36:59 +0800

    Output cleanup

--- src/main.rs
+++ src/main.rs
@@ -181,11 +181,10 @@ fn create_progress_bar(message: &str) -> ProgressBar {
     let pb = ProgressBar::new(100);
     pb.set_style(
         ProgressStyle::default_bar()
-            .template("{spinner:.green} {msg} [{elapsed_precise}] [{bar:40.cyan/blue}] {percent}%")
+            .template("{msg} [{elapsed_precise}] [{bar:43}] {percent}%")
             .unwrap()
-            .progress_chars("#>-"),
+            .progress_chars("█░"),
     );
     pb.set_message(message.to_string());
-    pb.enable_steady_tick(std::time::Duration::from_millis(100));
     pb
 }
